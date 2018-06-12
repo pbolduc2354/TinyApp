3d3430a1c53fa2f53deef2171fb52ad8be5fed26
Philippe Bolduc <pbolduc2354@gmail.com>
2018-06-12 22:37:33 +0000

    creating short url and being able to acces each url alone

---
+++ w2d2/project_TinyApp/express_server.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+var PORT = 8080;
+
+app.set("view engine", "ejs")
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xk": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>/n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let shortURL = req.params.id;
+  let longURL = urlDatabase[shortURL];
+  let templateVars = { shortURL : shortURL,
+    longURL: longURL
+   }
+  res.render("urls_show", templateVars);
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
