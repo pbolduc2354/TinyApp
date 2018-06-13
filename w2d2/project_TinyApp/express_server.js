39e5579fb13e5d8d9aa8c27333edd8bdb4b070c0
Philippe Bolduc <pbolduc2354@gmail.com>
2018-06-13 00:43:36 +0000

    can get information from the browser

--- w2d2/project_TinyApp/express_server.js
+++ w2d2/project_TinyApp/express_server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs")
 
 var urlDatabase = {
@@ -24,6 +27,10 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
